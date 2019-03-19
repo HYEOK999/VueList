66dcc49f9efeedd5030c4c08fa1086416bfb92bb
hyeok <iasg2004@naver.com>
2019-03-19 17:11:45 +0900

    footer태그 수정 및 Detail Value값 수정.

--- src/components/DetailPage.vue
+++ src/components/DetailPage.vue
@@ -5,36 +5,29 @@
             <div id="Categorybar">
                             <!--카데고리명 name, 카테코리번호 category_no랑 매칭 -->
                 <span class="card-title">
-                    <!-- 테스트 주석처리 -->
-                    {{detail_category[detail_list.category_no].name}}
-                    <!-- {{detail_category[$store.state.value_modal-1].name}} -->
+                    <!-- {{detail_category[detail_list.category_no].name}} -->
+                    {{detail_category[$store.state.value_modal-1].name}}
                 </span> 
                 <span class="card-title" align="right">
-                    <!-- 테스트 주석처리 -->
                     {{detail_list.no}}
                 </span> <!--글번호 no-->
             </div>
         </div>
         <div class="card-body" style="text-overflow: ellipsis;"><!----><!---->
             <span class="card-link">
-                <!-- 테스트 주석처리 -->
                 {{detail_list.email}}
             </span> <!--이메일 email-->
             <span class="card-link">
-                <!-- 테스트 주석처리 -->
                 {{detail_list.updated_at}}
             </span> <!--작성일 updated_at-->
             <br>
             <br>
             <h6 class="card-subtitle text-muted mb-2">
-                <!-- 테스트 주석처리 -->
                 {{detail_list.title}}
             </h6> <!--제목 title-->
                 <p class="card-text target">
-                    <!--내용 contents-->
-                    <!-- 테스트 주석처리 -->
                     {{detail_list.contents}} 
-                </p>
+                </p><!--내용 contents-->
         </div>        
     </div>
     <b-modal
@@ -53,18 +46,20 @@
         </b-card-text>
         <div style="border: 1px solid black; padding: 20px;">
               <b-card-text>
-                지금 가입하면 꿈 꾸던 기업에 재직 중인 현직자와 <span style="color:red; font-weight: bold;">익명</span>으로 대화 할 수 있습니다.
+                지금 가입하면 꿈 꾸던 기업에 재직 중인 현직자와 
+                <span style="color:red; font-weight: bold;">익명</span>
+                으로 대화 할 수 있습니다.
               </b-card-text>
         </div>
         <br>
         <div>
-            <b-button style="margin-bottom: 10px; background-color:#3b5988;" block variant="primary">페이스북으로 회원가입</b-button>
+            <b-button style="margin-bottom: 10px; background-color:#3b5988; border-width: 0px;" block variant="primary">페이스북으로 회원가입</b-button>
         </div>
         <div style="color:red;">
-            <b-button style="margin-bottom: 10px; background-color:#ea4335;" block>구글로 회원가입</b-button>
+            <b-button style="margin-bottom: 10px; background-color:#ea4335; border-width: 0px;" block>구글로 회원가입</b-button>
         </div>
         <div>
-            <b-button style="margin-bottom: 10px; background-color:#00bd39;" block variant="primary">네이버로 회원가입</b-button>
+            <b-button style="margin-bottom: 10px; background-color:#00bd39; border-width: 0px;" block variant="primary">네이버로 회원가입</b-button>
         </div>
       </b-container>
 
@@ -72,33 +67,18 @@
           <b-button size="sm" variant="light" @click="doDisplay()">나중에 하기</b-button>
       </div>
     </b-modal>
-    <div class="modal" id="join-induce-modal-footer-wrap">
-      <div class="modal-content join-induce-footer-modal">
-        <div class="modal-body">
-          <div class="join-induce-footer-box">
-            <div class="join-induce-footer-ment">
-              <p>지금 가입하면 꿈꾸던 기업에 재직 중인 현직자와 <span class="red-span">익명</span>으로 대화할 수 있습니다.</p>
-            </div>
-            <div class="join-btn-footer-wrap">
-              <button class="sns-join-btn">SNS계정으로 빠른 회원가입</button>
-              <button class="or">또는</button>
-              <button class="login-btn">로그인</button>
-            </div>
+
+      <div style="position: fixed; bottom: 0px; right: 0px; left: 0px; border-width: 0px; display: none;" id="footer">
+        <hr />
+          <div class="text-center">
+            지금 가입하시 꿈꾸던 기업에 재직 중인 현직자와
+            <span style="color: red">익명</span>으로 대화할 수 있습니다.<br />
+            <button type="button" class="btn btn-success btn-small">SNS 계정으로 빠른 회원가입</button>
+            <span> 또는 </span>
+            <button type="button" class="btn btn-default btn-small">로그인</button>
           </div>
         </div>
       </div>
-    </div>
-
-    <div id="footer">
-          <p>지금 가입하면 꿈 꾸던 기업에 재직 중인 현직자와 <span style="color:red; font-weight: bold;">익명</span>으로 대화 할 수 있습니다.</p>
-          <b-row>
-              <b-col>  <b-button variant="success">SNS계정으로 빠른 회원가입</b-button> </b-col>
-              <b-col cols="1"> 또는 </b-col>
-              <b-col>  <b-button variant="light">로그안</b-button> </b-col>
-          </b-row>
-    </div>
-
-  </div>
 </template>
 
 <script>
